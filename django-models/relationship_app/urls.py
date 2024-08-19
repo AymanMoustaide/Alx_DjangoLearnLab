2bf9ffe8573e95ce85f4ad1b5f3390babfd0e28a
AymanMoustaide <ayman.moustaide@icloud.com>
2024-08-19 12:30:19 +0100

    ::

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -30,3 +30,8 @@ urlpatterns = [
     
     # Other URL patterns...
 ]
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
